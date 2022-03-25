e29d49a8f11899867cf2b3f4441cc9833ab4f7a5
Alejandro Martín de León <alu01015941@ull.edu.es>
2022-03-25 20:58:27 +0000

    commented modification

--- tests/modificacion/arithmeticable.spec.ts
+++ tests/modificacion/arithmeticable.spec.ts
@@ -1,22 +1,22 @@
-import 'mocha';
-import {expect} from 'chai';
-import {ArithmeticableCollection} from '../../src/modificacion/arithmeticable';
+// import 'mocha';
+// import {expect} from 'chai';
+// import {ArithmeticableCollection} from '../../src/modificacion/arithmeticable';
 
-const numberCollection = new ArithmeticableCollection<number>([10, 2]);
+// const numberCollection = new ArithmeticableCollection<number>([10, 2]);
 
-describe('Arithmeticable Interface tests', () => {
-  describe('arithmeticable Collection methods', () => {
-    it('Interface methods', () => {
-      expect(numberCollection.getNumberOfArithmeticables).to.be.eql(2);
-      expect(numberCollection.getArithmeticable(0)).to.be.eql(10);
-    });
-  });
+// describe('Arithmeticable Interface tests', () => {
+//   describe('arithmeticable Collection methods', () => {
+//     it('Interface methods', () => {
+//       expect(numberCollection.getNumberOfArithmeticables).to.be.eql(2);
+//       expect(numberCollection.getArithmeticable(0)).to.be.eql(10);
+//     });
+//   });
 
-  describe('arithmeticable Collection methods', () => {
-    it('Interface methods', () => {
-      numberCollection.addArithmeticable(15);
-      expect(numberCollection.getNumberOfArithmeticables).to.be.eql(3);
-      expect(numberCollection.getArithmeticable(1)).to.be.eql(2);
-    });
-  });
-});
+//   describe('arithmeticable Collection methods', () => {
+//     it('Interface methods', () => {
+//       numberCollection.addArithmeticable(15);
+//       expect(numberCollection.getNumberOfArithmeticables).to.be.eql(3);
+//       expect(numberCollection.getArithmeticable(1)).to.be.eql(2);
+//     });
+//   });
+// });
