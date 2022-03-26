4c5eb4f653249d25b9d4aa7c5bd5b9b9b0a83a5b
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-03-26 23:39:22 +0000

    tests ready for serie

---
+++ tests/ejercicio-2/serie.spec.ts
@@ -0,0 +1,58 @@
+import 'mocha';
+import {expect} from 'chai';
+import {Serie} from '../../src/ejercicio-2/serie';
+
+const LCDP = new Serie('La Casa de Papel', 'Jesús Colmenar', 'Drama', 2017, 4.5);
+const Loki = new Serie('Loki', 'Kate Herron', 'SuperHéroes', 2021, 5);
+const HTGAWM = new Serie('How to get away with murder', 'Peter Nowalk', 'Accion', 2014, 4);
+
+describe('serie class tests', () => {
+  describe('serie constructor', () => {
+    it('serie empty object', () => {
+      const emptyserie = new Serie();
+      expect(emptyserie.title).to.be.eql('');
+      expect(emptyserie.director).to.be.eql('');
+      expect(emptyserie.genre).to.be.eql('');
+      expect(emptyserie.year).to.be.eql(0);
+      expect(emptyserie.ranking).to.be.eql(0);
+    });
+    it('LCDP serie object', () => {
+      expect(LCDP.title).to.be.eql('La Casa de Papel');
+      expect(LCDP.director).to.be.eql('Jesús Colmenar');
+      expect(LCDP.genre).to.be.eql('Drama');
+      expect(LCDP.year).to.be.eql(2017);
+      expect(LCDP.ranking).to.be.eql(4);
+    });
+    it('Loki serie object', () => {
+      expect(Loki.title).to.be.eql('Loki');
+      expect(Loki.director).to.be.eql('Kate Herron');
+      expect(Loki.genre).to.be.eql('SuperHéroes');
+      expect(Loki.year).to.be.eql(2021);
+      expect(Loki.ranking).to.be.eql(4);
+    });
+    it('LCDP serie object', () => {
+      expect(HTGAWM.title).to.be.eql('How to get away with murder');
+      expect(HTGAWM.director).to.be.eql('Peter Nowalk');
+      expect(HTGAWM.genre).to.be.eql('Accion');
+      expect(HTGAWM.year).to.be.eql(2014);
+      expect(HTGAWM.ranking).to.be.eql(5);
+    });
+  });
+  describe('serie getters', () => {
+    it('Get Tittle method', () => {
+      expect(HTGAWM.getTitle()).to.be.eql('How to get away with murder');
+    });
+    it('Get director method', () => {
+      expect(HTGAWM.getDirector()).to.be.eql('Peter Nowalk');
+    });
+    it('Get genre method', () => {
+      expect(HTGAWM.getGenre()).to.be.eql('Accion');
+    });
+    it('Get year method', () => {
+      expect(HTGAWM.getYear()).to.be.eql(2014);
+    });
+    it('Get ranking method', () => {
+      expect(HTGAWM.getRanking()).to.be.eql(5);
+    });
+  });
+});
