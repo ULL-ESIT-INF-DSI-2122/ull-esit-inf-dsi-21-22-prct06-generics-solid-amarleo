4a4d0e699a64bbff66ceab9ec5d2606f4a7d0e33
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-03-27 00:31:38 +0000

    alphabet tests ready

---
+++ tests/ejercicio-3/alphabet.spec.ts
@@ -0,0 +1,24 @@
+import 'mocha';
+import {expect} from 'chai';
+import {Alphabet} from '../../src/ejercicio-3/alphabet';
+
+const alphabet = new Alphabet('ABCDEFGHIJKLMNÑOPQRSTUVWXYZ');
+const emptyAlphabet = new Alphabet('');
+
+describe('Alphabet Class tests', () => {
+  describe('Alphabet Constructor', () => {
+    it('Alphabet atributtes must be accesible', () => {
+      expect(alphabet.content).to.be.eql('ABCDEFGHIJKLMNÑOPQRSTUVWXYZ');
+      expect(emptyAlphabet.content).to.be.eql('');
+    });
+  });
+  describe('Alphabet Methods', () => {
+    it('GetContent', () => {
+      expect(alphabet.getContent()).to.be.eql('ABCDEFGHIJKLMNÑOPQRSTUVWXYZ');
+    });
+    it('setContent', () => {
+      alphabet.setContent('ABCD');
+      expect(alphabet.getContent()).to.be.eql('ABCD');
+    });
+  });
+});
